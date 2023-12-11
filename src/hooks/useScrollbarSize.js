f52e755403af633e07279d0d7dcaa48719cb6d66
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-12-11 16:28:57 +0300

    feat: refactoring

--- src/hooks/useScrollbarSize.js
+++ src/hooks/useScrollbarSize.js
@@ -8,29 +8,23 @@ export const useScrollbarSize = () => {
 
   useEffect(() => {
     elementRef.current = document.createElement("div");
-    elementRef.current.style.width = "90px";
-    elementRef.current.style.height = "90px";
+    elementRef.current.style.width = "100px";
+    elementRef.current.style.height = "100px";
     elementRef.current.style.overflow = "scroll";
     elementRef.current.style.position = "absolute";
     elementRef.current.style.top = "-9999px";
-    elementRef.current.setAttribute("aria-hidden", "true");
     document.body.appendChild(elementRef.current);
 
-    const { offsetWidth, clientWidth } = elementRef.current;
+    const { offsetWidth, clientWidth, offsetHeight, clientHeight } =
+      elementRef.current;
     const width = offsetWidth - clientWidth;
-
-    const { offsetHeight, clientHeight } = elementRef.current;
     const height = offsetHeight - clientHeight;
 
     setSiza({ width, height });
 
-    console.log("After check", { width, height });
+    document.body.removeChild(elementRef.current);
 
-    return () => {
-      if (elementRef.current) {
-        document.body.removeChild(elementRef.current);
-      }
-    };
+    console.log("After check", { width, height });
   }, []);
 
   return size;
